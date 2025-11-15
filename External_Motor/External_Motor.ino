d49d36d3612d0631471acb9eba74462e12555635
giacomote <giacomotessari0@gmail.com>
2025-11-15 17:54:45 +0100

    Added 2 projects

---
+++ External_Motor/External_Motor.ino
@@ -0,0 +1,27 @@
+/*
+Control an external motor (fan) using an N-MOS transistor.
+
+The program receives an input string, which can be either "ON\n" or "OFF\n".
+If the string is "ON\n", the motor (fan) is switched on.
+If the string is "OFF\n", the motor (fan) is switched off.
+
+The circuit used is the one shown here: https://www.tinkercad.com/things/9lSNsUSi3QE-externalmotor
+*/
+
+String s;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(10, OUTPUT);
+
+  digitalWrite(10, LOW);
+}
+
+void loop() {
+  if (Serial.available()) {
+    s = Serial.readStringUntil('\n');
+  }
+
+  if (s == "ON") digitalWrite(10, HIGH);
+  else if (s == "OFF") digitalWrite(10, LOW);
+}
---
+++ External_Motor_PWM/External_Motor_PWM.ino
@@ -0,0 +1,31 @@
+/*
+Control an external motor (fan) using an N-MOS transistor and a PWM.
+
+The program receives an input number, which can be in the [0, 100] range.
+That number is the percentage of the maximum speed to run the motor (fan).
+
+NOTE: Pay attention not to send newline or carriage return at the end.
+
+The circuit used is the one shown here: https://www.tinkercad.com/things/9lSNsUSi3QE-externalmotor
+*/
+
+int duty;
+int pwmValue;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(10, OUTPUT);
+
+  digitalWrite(10, LOW);
+}
+
+void loop() {
+  if (Serial.available()) {
+    duty = Serial.parseInt();
+  }
+
+  duty = constrain(duty, 0, 100);  // fix "duty" in a certain range
+  pwmValue = map(duty, 0, 100, 0, 255);  // convert to percentage
+
+  analogWrite(10, pwmValue);  // activate PWM (default 490 Hz frequency)
+}
